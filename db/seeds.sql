331d9474af22be23a3b1b07c0eda666aa9a23142
Mikhail Metrikin <metrikin@gmail.com>
2017-04-30 11:04:38 -0700

    Minor sql fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,5 @@ INSERT INTO burgers(burger_name, devoured)
         ('Royale w/ Cheese', true),
         ('Krabbie Patty', false),
         ('Krusty Burger', false),
-        ("McDowell's Burger", false),
-        ("Cartman Burger", true)
+        ('McDowell''s Burger', false),
+        ('Cartman Burger', true)
