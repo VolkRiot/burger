243ce3ccf42710b42538203a791b14a8d1e84bce
Mikhail Metrikin <metrikin@gmail.com>
2017-05-04 12:27:23 -0700

    Finalized schema and heroku JAWSDB live

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
-DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 CREATE TABLE burgers(
-id INTEGER(11) AUTO_INCREMENT,
-burger_name TEXT not null,
-devoured BOOLEAN default FALSE,
-`date` TIMESTAMP not null,
-PRIMARY KEY (id)
+    id INTEGER(11) AUTO_INCREMENT,
+	burger_name TEXT not null,
+	devoured BOOLEAN not null default FALSE,
+	`date` TIMESTAMP not null,
+	PRIMARY KEY (id)
 );
